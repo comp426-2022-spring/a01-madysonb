c86e560f57ce316cb08ba792ab5833ebb8133eec
Madyson Barber <madysonb@live.unc.edu>
2022-01-25 18:57:41 -0500

    removed log(data)

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
         return
         process.exit(1)
     }
-    console.log(data)
+
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
